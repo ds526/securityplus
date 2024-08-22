635e303c858beb027e0c6c87d0ad7b2ccd666846
David Stuart <davidstuart199@gmail.com>
2024-08-21 22:13:04 -0400

    add missing quote

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running setup..."
-if [ $# -eq 0 ]
+if [ $# -eq 0 ];
   then
     echo "No arguments supplied"
-    echo "usage: ./setup.sh <imagename>
+    echo "usage: ./setup.sh <imagename>"
 fi
 docker build -t secplus.app --build-arg MY_IMAGE=$1
